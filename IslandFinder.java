22c542c4a7ab817a0e94d61c360a78edc7433b37
Abdi Ahmed <89213120+abdirashidexe@users.noreply.github.com>
2025-10-02 07:14:49 -0700

    steps 1-3 done

--- IslandFinder.java
+++ IslandFinder.java
@@ -8,7 +8,17 @@ public class IslandFinder {
      * @return number of distinct islands
      */
     public static int countIslands(int[][] grid) {
-        // TODO: implement
+
+        // step 1: base case (if grid is NULL or EMPTY) - if grid is null or has no rows, there can't be any islands, so just return 0 immediately.
+        if (grid == null || grid.length == 0) return 0;
+
+        // step 2: store the grid dimensions - needed when we loop later.
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        // step 3: make visited tracker - don't want to count same island more than 1x.
+        boolean[][] visited = new boolean[rows][cols];
+
         return 0;
     }
 }
